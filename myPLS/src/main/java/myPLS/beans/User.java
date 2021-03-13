cb4c8cf658198446702a410393962c3ab04a1cc1
ASHNIL VAZIRANI <av3977@g.rit.edu>
2021-03-13 13:18:55 -0500

    Group Module

--- myPLS/src/main/java/myPLS/beans/User.java
+++ myPLS/src/main/java/myPLS/beans/User.java
@@ -2,12 +2,20 @@ package myPLS.beans;
 
 
 public class User {
+	private int userID;
 	private String name;
 	private String email;
 	private boolean authorized;
 	private String role;
 	private String password;
 	
+	public User(){}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public int getUserID() {
+		return this.userID;
+	}
 	public String getName() {
 		return name;
 	}
@@ -38,5 +46,27 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
+	public User(int userID, String name, String email, boolean authorized, String role, String password) {
+		this.userID = userID;
+		this.name = name;
+		this.email = email;
+		this.authorized = authorized;
+		this.role = role;
+		this.password = password;
+	}
+	public User(String name, String email, boolean authorized, String role, String password) {
+		this.name = name;
+		this.email = email;
+		this.authorized = authorized;
+		this.role = role;
+		this.password = password;
+	}
+	public User(int userID, String name, String email, String role){
+		this.userID = userID;
+		this.name = name;
+		this.email = email;
+		this.role = role;
+		
+	}
 }
