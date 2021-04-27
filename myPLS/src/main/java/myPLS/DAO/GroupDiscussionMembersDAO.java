914b73a186cc606ab1c94f1ed85af3fe499dc741
Abriti <abriti@Abritis-MacBook-Pro.local>
2021-04-27 01:53:18 -0400

    added javadoc comments for DAO

--- myPLS/src/main/java/myPLS/DAO/GroupDiscussionMembersDAO.java
+++ myPLS/src/main/java/myPLS/DAO/GroupDiscussionMembersDAO.java
@@ -12,11 +12,22 @@ import myPLS.beans.GroupDiscussion;
 import myPLS.beans.User;
 import myPLS.services.UserService;
 
+/**
+ * This GroupDiscussionMembersDAO class to get details of member in a group
+ * @author ashnil
+ *
+ */
 public class GroupDiscussionMembersDAO {
     private static UserService userService;
     public GroupDiscussionMembersDAO(){
         userService = new UserService();
     }
+    
+    /**
+     * This getAllMembersInAGroup method will get details of all members in a group
+     * @param groupDiscussionID id of discussion group
+     * @return list of all users in the group
+     */
     public List<User> getAllMembersInAGroup(int groupDiscussionID){
         final String GET_USERS_IN_GROUP = "SELECT * FROM groupDiscussionMembers WHERE groupDiscussionID = "+groupDiscussionID;
 		List<User> usersInGroup = new ArrayList<>();
